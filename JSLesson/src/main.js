ed967a4e1be2a21771776f52f0edd94fbb9637d1
artem <vonap88@mail.ru>
2024-09-10 21:38:47 +0300

    JS

---
+++ JSLesson/src/main.js
@@ -0,0 +1,45 @@
+//alert("Hello from JavaScript!")
+//confirm("Согласны? Узнали?")
+// alert("Укажите вашеи имя")
+
+// let ans;
+// ans = prompt("Укажите вашеи имя");
+// alert("Ваше имя - " + ans);
+
+// let ans;
+// ans = prompt("Ведите код операции");
+// if (ans == '6453'){
+//     alert("Молодец! Правильный ответ");
+// }
+// else{
+//     alert("Код неверный! Не молодец ;с");
+//     location.reload();
+// }
+
+// let x;
+// x = prompt("Введите число");
+// x = Number(x);
+// alert("Ваше число в квадрате: " + x ** 2);
+
+// let x, y, s;
+// x = prompt("Введите 1 число");
+// y = prompt("Введите 2 число");
+// x = Number(x);
+// y = Number(y);
+// s = (x+y)/2;
+// alert("Среднее орифметическое ваших чисел: " + s);
+
+// 0.621371
+
+// const mile = 0.621371;
+// let km = Number(prompt("Введите значение в км"));
+// alert("В милях: " + km * mile + " миль");
+
+/*let x = prompt("Введите пятизначное число:");
+let last = x.slice(4);
+let res = last + x.slice(0,4);
+alert("Ваше число: " + res)*/
+
+let x = Number(prompt("Введите первое число"));
+let y = Number(prompt("Введите второе число"));
+alert("Сумма: "+ (x+y) + "\nРазность: " + (x-y) + "\nПроизведение: "+ x*y + "\nДеление: "+ x/y)
