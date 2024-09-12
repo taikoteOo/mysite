f1d9c9d621ea74994647b78c1d80caec7c7a7479
artem <vonap88@mail.ru>
2024-09-12 21:38:27 +0300

    JS2

--- JSLesson/src/main.js
+++ JSLesson/src/main.js
@@ -40,6 +40,55 @@ let last = x.slice(4);
 let res = last + x.slice(0,4);
 alert("Ваше число: " + res)*/
 
-let x = Number(prompt("Введите первое число"));
-let y = Number(prompt("Введите второе число"));
-alert("Сумма: "+ (x+y) + "\nРазность: " + (x-y) + "\nПроизведение: "+ x*y + "\nДеление: "+ x/y)
+// let x = Number(prompt("Введите первое число"));
+// let y = Number(prompt("Введите второе число"));
+// alert("Сумма: "+ (x+y) + "\nРазность: " + (x-y) + "\nПроизведение: "+ x*y + "\nДеление: "+ x/y)
+
+// window.onload = function () {
+//     function foo(){
+//         const k = 2.548;
+//         let a = Number(prompt("Введите первое число"));
+//         let x = Number(prompt("Введите второе число"));
+//         alert("y=kx+ax2\nk="+k+"\nx="+x+"\na="+a+"\ny="+k+"*"+x+"+"+a+"*"+x*x+"\ny="+(k*x+a*(x**2)));
+//     }
+//     foo();
+// }
+
+function calc(a, b, operation){
+    if (operation == "+") {
+      return a + b;
+    } else if (operation == "-") {
+      return a - b;
+    } else if (operation == "*") {
+      return a * b;
+    } else if (operation == "/") {
+      if (b==0){
+        return "Делить на ноль нельзя!";
+      } else {
+        return a / b;
+      }
+    } else {
+        return "Неизвестная операция. Попробуйте ещё раз";
+    }
+}
+
+// window.onload = ()=> {
+//     let x = Number(prompt("Введите первое число"));
+//     let y = Number(prompt("Введите второе число"));
+//     let op = prompt("Введите тип операции (+,-,*,/)");
+//     let res = calc(x, y, op);
+//     alert("Результат вашей операции: "+ res);
+// }
+
+function calcPaS() {
+    let x = Number(document.getElementsByName("nam1")[0].value);
+    let y = Number(document.getElementsByName("nam2")[0].value);
+    let op = document.getElementsByName("operation")[0].value;
+    let res = calc(x, y, op);
+    alert("Результат операции: " + res);
+}
+
+window.onload = ()=> {
+    let buttonCalc = document.getElementById("calculate");
+    buttonCalc.addEventListener("click", calcPaS);
+}
